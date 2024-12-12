6d02c4fcb575fc908621134c9663cda6cb0fc3f9
Minnie <rynaxd.com@gmail.com>
2024-12-13 00:34:20 +0700

    management

--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -3,7 +3,5 @@
 return [
     App\Providers\AppServiceProvider::class,
     App\Providers\AuthServiceProvider::class,
-    App\Providers\FortifyServiceProvider::class,
-    App\Providers\JetstreamServiceProvider::class,
     App\Providers\TelescopeServiceProvider::class,
 ];
